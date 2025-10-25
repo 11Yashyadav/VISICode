414024a4014b18f02dd84bad072e779f057c578f
Yash Yadav <yashyadav9325@gmail.com>
2025-10-25 18:58:06 +0530

    Configration for Classical Algorithms Created

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+const withMDX = require("@next/mdx")();
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+export default nextConfig;
+module.exports = withMDX(nextConfig);
